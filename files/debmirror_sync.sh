e4309c898edbef08addcff2020bb6ad48f368371
s3tupw1zard <manuel.merkl@proton.me>
2024-07-12 17:50:07 +0200

    Try to fix error with gpg

--- files/debmirror_sync.sh
+++ files/debmirror_sync.sh
@@ -1,1 +1,4 @@
-/usr/bin/debmirror -v --config-file=${CONFDIR}/debmirror.conf ${MIRRORDIR} # >> ${CONFDIR}/${DEBUGFILE} 2>&1
+/usr/bin/gpg --keyring /usr/share/keyrings/ubuntu-archive-keyring.gpg --export \
+| /usr/bin/gpg --no-default-keyring --keyring trustedkeys.gpg --import
+
+usr/bin/debmirror -v --config-file=${CONFDIR}/debmirror.conf ${MIRRORDIR} # >> ${CONFDIR}/${DEBUGFILE} 2>&1
